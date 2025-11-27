c109db58efbd9a0936aa67d18b7daee0ad5fa7b5
Andrew Shilman <Andrew.Shilman@gmail.com>
2025-11-27 15:43:40 -0800

    added base react files for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto p-8">
+
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          SchemaSense
+        </h1>
+
+      </div>
+    </div>
+
+
+
+  )
+}
+
+export default App
